0e3b9965713ea998983e95917a1b6f208741d03e
Fahad <fahad.alsalihi05@gmail.com>
2023-12-01 15:14:59 -0800

    finished it in 7 minutes

--- temperatureConversionCalculator.c
+++ temperatureConversionCalculator.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
-#include<math.h>
+#include<ctype.h>
 //im gonna make a temerature conversion calculato
 int main(){
 
+char unit;
+float temp;
+
+printf("is the temperature in (F) or (C)? ");
+scanf("%c", &unit);
+unit = toupper(unit);
+
+if(unit == 'C'){
+printf("\nenter the tempurature in celsius ");
+scanf("%f", &temp);
+temp = (temp * 9 / 5) + 32;
+printf("\n the temp in farenheit is: %.1f", temp);
+}
+
+else if(unit == 'F'){
+printf("\nenter the tempurature in farenheit ");
+scanf("%f", &temp);
+temp = ((temp - 32 )* 5) / 9;
+printf("\nthe temp in celsius is %f", temp);
 
 
 
+}
 
+else{
+    printf("\n %c is not a valid choice", unit);
 
-
-
-
+}
 
 
 
